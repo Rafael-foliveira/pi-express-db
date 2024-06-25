adb70ef41a006c0812a18e83fcea8f674d463988
rafaelfariasoliver <rafaelfariasdeoliveira310@gmail.com>
2024-06-25 17:19:52 -0300

    salvando alteraçoes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const data = {
+    title: 'CR7',
+    name: 'Rafael'
+  }
+  res.render('index', data);
 });
 
 module.exports = router;
