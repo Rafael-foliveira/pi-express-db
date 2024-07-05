712f6d2116691696982fd67b16f27a36af8375e0
rafaelfariasoliver <rafaelfariasdeoliveira310@gmail.com>
2024-07-05 17:33:22 -0300

    aula da sexta05

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var alunosRouter = require('./routes/alunos');
+var httpoverrideHttpMethod = require('./middleware/http-method-override')
+
 
 var app = express();
+app.use(bodyParser.urlencoded());
+
+app.use(httpoverrideHttpMethod)
 
 var { create } = require('express-handlebars');
 var hbs =  create({ extname: '.hbs' })
--- routes/alunos.js
+++ routes/alunos.js
@@ -14,6 +14,10 @@ router.get('/new', function(req, res, next){
     res.render('form', { title: 'Novo Aluno', buttonText: 'Adcionar'});
 
 })
+router.post('/', function (req, res, next) {
+    const {body, method} = req
+    res.send({body,method});
+});
 router.post('/create', function(req, res, next){
     const novoAluno = req.body;
     const matricula = novoAluno.matricula;
@@ -40,5 +44,15 @@ router.get('/edit/:matricula', function(req, res, next){
     res.render('form', { title: 'Editar Aluno', buttonText:'Salvar Alterações', aluno});
 
 })
+router.put('/', function (req, res, next) {
+    const {body, method} = req
+    res.send({body,method,msg:'altera usuario'});
+});
+router.delete('/', function (req, res, next) {
+    const {body, method} = req
+    res.send({body,method ,msg:'remover aluno'});
+});
+
+
 
 module.exports = router;
